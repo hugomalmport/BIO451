3475a391afa88089c1f590c4917f2246491092fa
hugomalmport <hugomalmport7@gmail.com>
2024-04-24 11:36:41 +0200

    Cleaning script - not finished yet

--- Spiralis_cleaning_script.R
+++ Spiralis_cleaning_script.R
@@ -7,13 +7,16 @@ rm(list=ls())
 
 #Read the excel file from its path
 ##Make sure to change path depending on where you saved it!
-###Change sheet depending on which sheet is going to be used!!
+###Change sheet depending on which sheet is going to be used!! - so either "Data" or "IC" (Initial chlorophyll)!!
 data_spiralis <- read_excel("C:\\Users\\hugom\\OneDrive\\Skrivbord\\Skolrelaterat\\BIO451\\Spiralis_data.xlsx", sheet = "Data")
 
 #Inspect the file
 summary(data_spiralis)
 head(data_spiralis)
 str(data_spiralis)
+
+#Remove columns with NA values
+data_spiralis <- data_spiralis[, colSums(is.na(data_spiralis)) == 0]
 
 #Remove rows with NA values
 ##Now it'll remove everything cause we have NA values in all rows
@@ -23,18 +26,23 @@ data_spiralis <- na.omit(data_spiralis)
 summary(data_spiralis)
 head(data_spiralis)
 
-#Separate data depending on column
-##Use 'ungroup()' if not necessary
-grouped_data <- data_spiralis %>%
-  group_by(chlorophyll)
+#Separate and organize data depending on column
+    ##Here I organize after the tank_id - 1-10
+###Use 'ungroup()' if not necessary after - or remove
+organized_data <- data_spiralis %>%
+  group_by(tank_id) %>%
+  arrange(tank_id)
 
-#Check the grouped data
-summary(grouped_data)
-
-#Remove columns with NA values
-data_spiralis <- data_spiralis[, colSums(is.na(data_spiralis)) == 0]
+#Check the grouped and organized data
+summary(organized_data)
 
 #Now we removed columns with NA values
 ##Check the data frame
 print(data_spiralis)
+
+#Decide the file name and path
+file_path <- "C:\\Users\\hugom\\OneDrive\\Skrivbord\\Skolrelaterat\\BIO451\\cleaned_data.csv"
+
+#Write the dataframe to a CSV file 
+write.csv(data_spiralis, file = file_path, row.names = FALSE)
 
